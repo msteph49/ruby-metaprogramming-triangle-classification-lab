2023ee3a22e298cee76a36376fc4945d6fdc9ead
Michael Stephens <mdstephens26@yahoo.com>
2020-12-21 22:15:25 -0500

    commit tree b2ca8328478937ed2cfad38aa589cab84389142f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
   # write code here
+  attr_accessor :side_a, :side_b, :side_c
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+  def valid?
+    if (side_a + side_b) > side_c && (side_b + side_c) > side_a && (side_a + side_c) > side_b && side_a >= 0 && side_b >= 0 && side_c >= 0 
+      true
+    else
+      false
+    end
+  end
+  def kind
+    if !valid?
+      raise TriangleError
+    elsif
+      side_a == side_b && side_b == side_c && side_a ==side_c
+      :equilateral
+    elsif
+      side_a == side_b || side_a == side_c || side_b == side_c
+      :isosceles
+    else
+      :scalene
+
+      
+    end
+  end
+
+
+  class TriangleError < StandardError
+  end
 end
